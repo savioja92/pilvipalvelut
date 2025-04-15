fd1d91e759a165dfdf3a058013f76755cd0d8ed8
savioja92 <niko.savioja@gmail.com>
2025-04-15 11:17:53 +0300

    yeah whatever

--- NikonWebsovellus/src/App.tsx
+++ NikonWebsovellus/src/App.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState, useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './vite.svg'
-import './App.css'
+import { useEffect, useState, useCallback } from 'react';
+import reactLogo from '/assets/react.svg';
+import viteLogo from '/vite.svg'
+import '/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
